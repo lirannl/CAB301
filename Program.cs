a5ff74c0b0f9150943c4eefa27d6a59dab918f0a
lirannl <liranpiade@gmail.com>
2021-04-21 18:05:28 +1000

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CAB301
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
